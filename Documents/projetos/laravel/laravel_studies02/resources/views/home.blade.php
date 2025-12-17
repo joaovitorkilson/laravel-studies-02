31b22356bf076ce7f2b7b302f126dac825210b79
joaovitorkilson <joaovitorkilson@gmail.com>
2025-12-17 10:32:38 -0300

    001

---
+++ Documents/projetos/laravel/laravel_studies02/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main_layout')
+
+@section('page_title', 'Home Page')
+
+
+@section('content')
+    <p>Conteúdo da minha view</p>
+@endsection
---
+++ Documents/projetos/laravel/laravel_studies02/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function() {
+    echo 'blade layouts';
+});
+
+Route::view('/home', 'home');
+Route::view('/other', 'other');
