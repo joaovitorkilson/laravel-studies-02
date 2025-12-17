0f0e65fb8a058c23ca0839085277f0eca855f799
joaovitorkilson <joaovitorkilson@gmail.com>
2025-12-17 11:00:17 -0300

    003

--- Documents/projetos/laravel/laravel_studies02/resources/views/home.blade.php
+++ Documents/projetos/laravel/laravel_studies02/resources/views/home.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.main_layout')
 
-@section('page_title', 'Home Page')
-
-
 @section('content')
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col card p-5 bg-secondary text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            </div>
-        </div>
-    </div>
+    <h3>Este texto faz parte da view</h3>
+
+    {{-- renderizar o component --}}
+    <x-my-component />
+
+    {{-- renderizar componente dentro de pasta --}}
+    <x-admin.admin-card />
+
+    <h3>Este texto faz parte da view</h3>
 @endsection
