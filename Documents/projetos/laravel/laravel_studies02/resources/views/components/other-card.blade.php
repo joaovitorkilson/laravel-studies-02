2c9b628862fcdad91d9a92366462bd0042fc9684
joaovitorkilson <joaovitorkilson@gmail.com>
2025-12-18 10:21:15 -0300

    007

---
+++ Documents/projetos/laravel/laravel_studies02/resources/views/components/other-card.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h3>Este é um texto inicial do componente</h3>
+
+    {{-- vamos exibir o conteúdo do slot aqui com a diretiva do blade --}}
+    {{ $slot }}
+
+    <h3>texto final do componente</h3>
+
+</div>
--- Documents/projetos/laravel/laravel_studies02/resources/views/home.blade.php
+++ Documents/projetos/laravel/laravel_studies02/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main_layout')
 @section('content')
 
-    <div class="text-center">
+    <!-- <div class="text-center">
     @foreach ($pessoas_linguas as $pessoa => $linguas)
     
     {{-- <x-card-pessoa :nome-pessoa="$pessoa" :linguas-pessoa="$linguas"/> --}} 
@@ -9,6 +9,40 @@
     <x-card-pessoa :$pessoa :$linguas/>
 
     @endforeach
-    </div>
+    </div> -->
+
+
+    {{-- componentes e slots --}}
+
+    <!-- <div>
+        <h4 class="text-info">Como funciona um Slot?</h4>
+        <x-other-card>
+            <h1 class="text-danger">Este é o Slot!</h1>
+        </x-other-card>
+    </div> -->
+
+    <!-- <x-multi-slot>
+        <x-slot:title>Este é o título</x-slot>
+        <x-slot:content>Este é o conteúdo</x-slot>
+        <x-slot:footer>
+            <ul>
+                <li>ítem 1</li>
+                <li>ítem 2</li>
+                <li>ítem 3</li>
+            </ul>
+        </x-slot>
+    </x-multi-slot> -->
+
+
+<h4>Componente anônimo</h4>
+<x-alert-card>
+    Primeira mensagem
+</x-alert-card>
+<x-alert-card>
+    Segunda mensagem
+</x-alert-card>
+<x-alert-card>
+    Terceira mensagem
+</x-alert-card>
 
 @endsection
